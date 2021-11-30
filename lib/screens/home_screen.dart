76e72dca48af8efb56b40429f107a210e20a9db3
balinux <ryojuniyantara@gmail.com>
2021-12-01 06:47:09 +0800

    update:

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,59 +9,55 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   double kdefaultPaddig = 20;
-  Color colorSetter = Colors.red;
+  Color colorSetter = Colors.white;
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Color Filter"),
-      ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(kdefaultPaddig),
-            width: size.width,
-            child: Column(
-              children: [
-                Container(
-                  child: ColorFiltered(
-                    child: Image(
-                      image: AssetImage("assets/images/rod-long.jpg"),
-                    ),
-                    colorFilter: ColorFilter.mode(colorSetter, BlendMode.color),
-                  ),
-                ),
-                Divider(
-                  height: kdefaultPaddig,
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        colorSetter = Colors.blue;
-                      });
-                    },
-                    child: Text("change to blue")),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        colorSetter = Colors.red;
-                      });
-                    },
-                    child: Text("change to red")),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        colorSetter = Colors.yellow;
-                      });
-                    },
-                    child: Text("change to yellow")),
-              ],
+        appBar: AppBar(
+          title: Text("Color Filter"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: size.width,
+              height: size.height * 0.3,
+              color: Colors.blue,
+              child: ColorFiltered(
+                child: Image(image: AssetImage("assets/images/rod-long.jpg")),
+                colorFilter: ColorFilter.mode(colorSetter, BlendMode.color),
+              ),
             ),
-          )
-        ],
-      ),
-    );
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    colorSetter = Colors.blue;
+                  });
+                },
+                child: Text("Blue")),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    colorSetter = Colors.red;
+                  });
+                },
+                child: Text("Red")),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    colorSetter = Colors.green;
+                  });
+                },
+                child: Text("Green")),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    colorSetter = Colors.yellow;
+                  });
+                },
+                child: Text("Yellow")),
+          ],
+        ));
   }
 }
